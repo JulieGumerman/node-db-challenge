9e94a59230370255f9c0c7e36d163ae78eeec9c6
Julie Gumerman <julie.gumerman@gmail.com>
2019-10-11 11:07:51 -0600

    can get and add tasks

--- routes_models/project-models.js
+++ routes_models/project-models.js
@@ -7,8 +7,25 @@ const getProjects = () => {
 const addProject = (project) => {
     return db("projects").insert(project)
 }
+/*********retrieve project by id, retrieve tasks *************/
+
+const getProjectById = (id) => {
+    return db("projects").where({id}).first()
+}
+
+const addTask = (task) => {
+    return db("tasks").insert(task)
+}
+const getTasks = (id) => {
+    return db("tasks").where("project_id", "=", id)
+}
+
+/*********export those functions!!!**************************/
 
 module.exports = {
     getProjects,
-    addProject
+    addProject,
+    getProjectById,
+    addTask, 
+    getTasks
 }
